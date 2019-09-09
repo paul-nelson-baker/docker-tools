c444ce2452921305aed7fba3a908d148d2f3c7e3
Paul Baker <paul.nelson.baker@gmail.com>
2019-09-09 14:19:48 -0600

    Creating a simple library for being lazy with docker

---
+++ docker-tools.go
@@ -0,0 +1,46 @@
+package docker_tools
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	dockerMachineHelper "github.com/paul-nelson-baker/docker-machine-helper"
+	"io"
+	"time"
+)
+
+type LazyDockerClient struct {
+	*client.Client
+	Timeout time.Duration
+}
+
+func NewLazyClient() (LazyDockerClient, error) {
+	if client, err := dockerMachineHelper.GetDockerClientEnvFallback(); err != nil {
+		return LazyDockerClient{}, err
+	} else {
+		return LazyDockerClient{
+			Client:  client,
+			Timeout: 0,
+		}, nil
+	}
+}
+
+func (c LazyDockerClient) LazyPull(image, version string) (io.ReadCloser, error) {
+	return c.LazyLibraryPull(`docker.io/library`, image, version)
+}
+
+func (c LazyDockerClient) LazyLibraryPull(library, image, version string) (io.ReadCloser, error) {
+	fullyQualifiedImageName := fmt.Sprintf("%s/%s:%s")
+	ctx, cancelFunc := c.newLazyContext()
+	defer cancelFunc()
+	return c.ImagePull(ctx, fullyQualifiedImageName, types.ImagePullOptions{
+		All:           false,
+		RegistryAuth:  "",
+		PrivilegeFunc: nil,
+	})
+}
+
+func (c LazyDockerClient) newLazyContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), c.Timeout)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/paul-nelson-baker/docker-tools
+
+go 1.13
+
+require github.com/paul-nelson-baker/docker-machine-helper v0.0.0-20190909022446-2876d3f5d9d5 // indirect
