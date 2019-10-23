d85c2c4320873bdb99c588dfcafc7636733a0937
Paul Baker <paul.nelson.baker@gmail.com>
2019-10-23 11:49:05 -0600

    Fixing. Gitignore was too strict

---
+++ build/build.go
@@ -0,0 +1,49 @@
+package build
+
+import (
+	"github.com/jhoonb/archivex"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+type TarFileConsumer func(tar *archivex.TarFile) error
+
+func CreateTarBuildContext(tarFileConsumers ...TarFileConsumer) (string, error) {
+	tarTempFile, err := ioutil.TempFile("", "*.tar")
+	if err != nil {
+		return "", err
+	}
+	tarFile := new(archivex.TarFile)
+	defer func() {
+		_ = tarFile.Close()
+	}()
+	tarTempFilename := tarTempFile.Name()
+	if err := tarFile.Create(tarTempFilename); err != nil {
+		return "", err
+	}
+	for _, tarFileConsumer := range tarFileConsumers {
+		if err := tarFileConsumer(tarFile); err != nil {
+			return "", err
+		}
+	}
+	return tarTempFilename, nil
+}
+
+type EventFunc func(event Event) error
+
+//type Event struct {
+//	Stream string `json:"stream"`
+//}
+
+type Event map[string]string
+
+// Logs any status or progress changes to the console via `log.Println`
+func LoggingFunc(event Event) error {
+	if output := strings.TrimSpace(event["stream"]); output != "" {
+		log.Println(output)
+	} else if len(event) > 1 {
+		log.Printf("%+v", &event)
+	}
+	return nil
+}
